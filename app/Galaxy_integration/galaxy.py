fc852b15991f682a4531810de50635fb27fbeb95
kewserseid <kewerseid91@gmail.com>
2025-07-18 15:22:17 +0300

    adding emit message to hypothesis and galaxy modules

--- app/Galaxy_integration/galaxy.py
+++ app/Galaxy_integration/galaxy.py
@@ -1,16 +1,18 @@
 
+from app.socket_manager import emit_to_user
 import requests
 import os
 
 GALAXY_MESSAGE_URL= os.getenv("GALAXY_MESSAGE_URL")
 class GalaxyHandler:
     def __init__(self, llm) -> None:
-        self.llm = llm  # You might store this for other uses
+        self.llm = llm 
 
     def get_galaxy_info(self, query, user_id):
         payload = {
             "message": query
         }
+        emit_to_user(user=user_id,message='Galaxy is processing your query in real time.')
         response = requests.post(GALAXY_MESSAGE_URL, json=payload)
         if response.status_code == 200:
             return response.json().get("response")
